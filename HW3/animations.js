166d9c4d94fe698c9ef22b351609dce05fa99477
yunus <yunus.sumer@stu.fsm.edu.tr>
2022-11-23 19:27:32 +0300

    hw3

--- HW3/animations.js
+++ HW3/animations.js
@@ -7,30 +7,31 @@ canvas.height = canvas.height = 300;
 //create empty array
 var images = [];
 images.length = 5;
-var myImage = null;
-const imageUrl = '/sprite/1.jpg';
+const imageUrl = "https://yunussmr.github.io/BlmAdvancedPrgrmng/HW3/sprite/2.jpg";
 
-
-fetch(imageUrl).then(res => res.blob()) // Gets the response and returns it as a blob 
+fetch(imageUrl).then(res => res.blob())
     .then(blob => {
         let objectURL = URL.createObjectURL(blob);
-        console.log("objectURL", objectURL);
         usableImage = objectURL;
-        myImage.src = usableImage;
+        imgObj.src = usableImage;
+        console.log(imgObj.src);
+        c.drawImage(imgObj, 100, 100, 100, 100);
     });
 
+var imgObj = null;
+var animate;
 
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+
+
+
+imgObj = new Image();
+imgObj.src = imageUrl;
 for (var i = 1; i < images.length; i++) {
     images[i] = new Image();
     images[i].src = 'sprite/' + i.toString() + '.jpg';
 }
-var i = 1;
-
-setInterval(function() {
-    i++;
-    if (i >= 6) {
-        i = 1;
-    }
-    c.drawImage(fetch(imageUrl), 100, 100, 100, 100);
-
-}, 500)
