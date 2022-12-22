a3e449969d0244089a0db08badebe523c749b990
yunus <yunus.sumer@stu.fsm.edu.tr>
2022-12-23 02:14:24 +0300

    project v.4

--- Project/app.js
+++ Project/app.js
@@ -18,40 +18,47 @@ document.addEventListener('DOMContentLoaded' , () => {
     let gravity = 1
     let isGameOver = false
     let gap = 430
-    let isClick = false
+    var score = 0;
+    var high=[];
+    let timer=20;
+    let speed=3000;
+    document.getElementById('high').innerHTML =(getMax(JSON.parse(localStorage.getItem("high"))));
   
     function startGame() {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
     }
-    let gameTimerId = setInterval(startGame, 20)
 
+    function getMax(arr) {
+        if (!arr) {
+          return null;
+        }
+       
+        var maxV = arr[0];
+        for (a of arr) {
+          if (a > maxV) maxV = a;
+        }
+        return maxV;
+      }
+      
+    let gameTimerId = setInterval(startGame, 20)
+    
     function control(e) {
-        if (e.keyCode === 32|| isClick==true) {
+        if (e.keyCode === 32) {
             jump()
         }
     }
-
-    function Click() {
-        gameDisplay.onclick = function () {
-          isClick = true
-        // console.log('Click just happened')
-         // console.log(isClick)
-        }
-    }
-      Click()
 
     function jump() {
         if (birdBottom < 500) birdBottom += 50
         bird.style.bottom = birdBottom + 'px'
         //console.log(birdBottom)
     }
-    document.addEventListener('keyup', control)
-    document.addEventListener('touchend',control)
-    document.addEventListener('keyup', reGame)
-    document.addEventListener('click', control)
 
+    document.addEventListener('keyup', control)
+    document.addEventListener('click', jump)
+    document.addEventListener('keyup', reGame)
     function generateObstacle() {
         let obstacleLeft = 400
         let randomHeight = Math.random() * 60
@@ -79,18 +86,29 @@ document.addEventListener('DOMContentLoaded' , () => {
                 gameDisplay.removeChild(obstacle)
                 gameDisplay.removeChild(topObstacle)
             }
+            if (obstacleLeft===40) {
+                score++
+                document.getElementById('score').innerHTML = score;
+            }
             if (
-               
                 obstacleLeft > 100 && obstacleLeft < 160 && birdLeft === 100 &&
-                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200)||
+                (birdBottom < obstacleBottom + 150 || birdBottom > obstacleBottom + gap -200)||
                 birdBottom === 0 
                 ) {
+                high=JSON.parse(localStorage.getItem("high"));
+                high.push(score);
+                localStorage.setItem('high', JSON.stringify(high));
                 gameOver()
                 clearInterval(timerId)
             }
         }
-        let timerId = setInterval(moveObstacle, 20) 
-        if (!isGameOver) setTimeout(generateObstacle, 3000)
+        let timerId = setInterval(moveObstacle, timer) 
+        if (!isGameOver) setTimeout(generateObstacle, speed)
+        if (score%3==0&&score!=0) {
+            timer-=5
+            speed-=500
+            console.log(timer);
+        }
 
     }
     generateObstacle()
@@ -101,20 +119,16 @@ document.addEventListener('DOMContentLoaded' , () => {
         console.log('game over')
         isGameOver = true
         document.removeEventListener('keyup', control)
-        document.removeEventListener('click', control)
-        document.removeEventListener('touchend',control)
+        document.removeEventListener('click', jump)
         ground.classList.add('ground')
         ground.classList.remove('ground-moving')
     }
 
     function reGame(e) {
         if (e.keyCode == 13) {
-        
-            this.location.reload();
+           this.location.reload();
            console.log("new game")
-           
-        }
-        
+        } 
     }
 
 })
